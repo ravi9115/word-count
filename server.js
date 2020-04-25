a35100ece16c2380656d24fc2f1ab4f347c0dfcb
ravi jain <ravi9115ravi@gmail.com>
2020-04-25 19:57:16 +0530

    change server file

--- server.js
+++ server.js
@@ -1,17 +1,6 @@
 const express = require('express');
 const path = require('path');
 
-const app = express();
-
-app.use(express.static(__dirname + '/dist'));
-
-app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname,'/dist/index.html'));
-});
-
-
-app.listen(process.env.PORT || 8080);
-
 const forceSSL = function() {
   return function (req, res, next) {
     if (req.headers['x-forwarded-proto'] !== 'https') {
@@ -23,4 +12,14 @@ const forceSSL = function() {
   }
 }
 
+const app = express();
+
+app.use(express.static(__dirname + '/dist/word-count'));
+
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname,'/dist/word-count/index.html'));
+});
+
 app.use(forceSSL());
+
+app.listen(process.env.PORT || 8080);

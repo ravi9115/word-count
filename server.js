51bea24de84178b0052413cb8e1ea28fd7e1af52
ravi jain <ravi9115ravi@gmail.com>
2020-04-25 16:55:51 +0530

    server changes

--- server.js
+++ server.js
@@ -5,10 +5,22 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist'));
 
-// app.get('/*', function(req,res) {
-    
-// res.sendFile(path.join(__dirname,'/dist/word-count/index.html'));
-// });
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname,'/dist/index.html'));
+});
 
 
 app.listen(process.env.PORT || 8080);
+
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+
+app.use(forceSSL());
